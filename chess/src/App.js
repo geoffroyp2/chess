a58dfbcfe2184658cf3574047fbd4cfb645f5aff
geoffroyp2 <g.poyferre@gmail.com>
2020-09-30 19:32:41 +0200

    v.0
    init

---
+++ chess/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./board/board";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      YO
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ chess/src/board/board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Square from './square'
+
+
+function Board() {
+    return (
+        <>
+            Board
+            <Square />
+        </>
+    )
+};
+
+export default Board;
